1f1029bd767e56d3ac087f274dac7026691253d5
Kerem <akd.kerem@gmail.com>
2021-11-10 00:28:33 +0100

    new class which generates matrix containing the distance between closed loops

--- dfs.py
+++ dfs.py
@@ -8,6 +8,9 @@ class Graph():
         """
         self.graph = adj_matrix
         self.V = self.graph.shape[0]
+        self.clear()
+
+    def clear(self):
         self.visited = [False] * self.V
         self.count = 0
         self.paths = []
@@ -33,21 +36,18 @@ class Graph():
         
         self.visited[node] = False
         return
-    
     def find_cycles(self, length):
         """
         searches for all closed cycles in graph by iterating 
         over V - (n+1) nodes if path lenght is n 
         """
-        self.count = 0
+        self.clear()
         for start_node in range(self.V - (length - 1)):
             self.DFS(length-1, start_node, start_node, path = [start_node])
             self.visited[start_node] = True
-
     def cyclic_permutation(iself, path):
        rev_list = list(reversed(path))
        return [rev_list.pop()] + rev_list
-            
     def drop_duplicates(self):
         """each cycle is counted twice, 
         here one count is removed"""
@@ -55,7 +55,10 @@ class Graph():
             cycl_perm = self.cyclic_permutation(p)
             if cycl_perm in self.paths:
                 self.paths.remove(cycl_perm)
-
+    # TODO: count degeneracies D in graph
+    def num_constrains(self):
+        """ C = K - N + 1"""
+        return int(self.graph.sum() / 2 - self.V + 1)
     @classmethod
     def fully(cls, n):
         """ generates fully connected graph with n nodes"""
@@ -65,3 +68,45 @@ class Graph():
     def random(cls, n):
         """generates graph with n nodes and random connections"""
         return cls(np.random.randint(2, size=(n,n)))
+
+class Cycle_Graph():
+
+    def __init__(self, cycl3, cycl4):
+        
+        self.sets = self.concatenate(
+                cycl3, cycl4)
+
+    def concatenate(self, cycl3, cycl4):
+        return cycl3 + cycl4
+
+    def cycle_adj_matrix(self):
+        N = range(len(self.sets))
+        matrix = [[self.distance(i, j) if i > j else 0 for i in N]
+                for j in N]
+        return matrix
+
+    def distance(self, i, j):
+       """measure the distance between 
+       two closed loops"""
+       cycli = self.get_list_of_neigb_tuples(i)
+       cyclj = self.get_list_of_neigb_tuples(j)
+       #return len(set(cycli) & set(cyclj))
+       intersection = (map(lambda x: ''.join(map(str, x)),(set(cycli) & set(cyclj))))
+       return ','.join(list(intersection))
+
+    def get_list_of_neigb_tuples(self, index):
+        cycle = self.sets[index]
+        cycle = cycle + [cycle[0]]
+        cycle = list(map(tuple, 
+            map(sorted, zip(cycle, cycle[1:]))))
+        return cycle
+
+    def print_matrix(self, matrix):
+        """ from stack overflow 13214809"""
+        s = [[str(e) for e in row] for row in matrix]
+        lens = [max(map(len, col)) for col in zip(*s)]
+        fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
+        table = [fmt.format(*row) for row in s]
+        print ('\n'.join(table))
+
+
