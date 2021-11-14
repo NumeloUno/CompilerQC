94ea65de13d5a3b42a7a2fae879b2910e4fd14c3
Kerem <akd.kerem@gmail.com>
2021-11-14 01:55:19 +0100

    rasterization of polygons and envelop polygon added

--- energy.py
+++ energy.py
@@ -1,16 +1,31 @@
+import numpy as np
 from polygons import Polygons
+from shapely.geometry import Polygon, MultiPoint
 
 class Energy(Polygons):
 
-    def __init__(self, polygon_coords):
-        self.polygon_coords = polygon_coords
+    def __init__(self, polygon_object: Polygons):
+        self.polygon_coords = polygon_object.get_all_polyg_coords()
+        self.envelop_polygon = polygon_object.convex_hull()
+        self.K = polygon_object.K
     def scopes_of_polygons(self):
-        return list(map(self.polygon_scope, self.polygon_coords))
+        return list(map(self.polygon_length, self.polygon_coords))
     def area_of_polygons(self):
         return list(map(self.polygon_area, self.polygon_coords))
-    
+    def is_plaquette(self):
+        unit_square = list(map(self.is_unit_square, self.polygon_coords))
+        unit_triangle = list(map(self.is_unit_triangle, self.polygon_coords))
+        return list(map(lambda x: True in x, zip(unit_square, unit_triangle)))
+    def intersection_array(self):
+        unit_square_grid = self.get_grid_of_unit_squares(self.K)
+        intersects = list(map(lambda x: Polygon(self.envelop_polygon).intersects(Polygon(x)),
+            unit_square_grid))
+        touches = list(map(lambda x: Polygon(self.envelop_polygon).touches(Polygon(x)),
+            unit_square_grid))
+        return np.logical_xor(intersects, touches)
+
     # TODO: energie terme aufstellen
 
     def __call__(self):
-        energy = sum(self.area_of_polygons())
+        energy = self.polygon_area(self.envelop_polygon)
         return energy
--- polygons.py
+++ polygons.py
@@ -1,13 +1,17 @@
 import numpy as np
 from matplotlib import pyplot as plt
-from dfs import Graph
-from shapely.geometry import Polygon
+from graph import Graph
+from shapely.geometry import Polygon, MultiPoint, MultiLineString
+from shapely.ops import polygonize
 
 class Polygons():
     def __init__(self, logical_graph: Graph):
         self.qbits = logical_graph.qubits_from_graph()
         self.K = len(self.qbits)
         self.qbit_coord_dict = self.init_coords_for_qbits()
+        cycles = (logical_graph.get_cycles(3)
+                + logical_graph.get_cycles(4))
+        self.polygons = self.get_all_polygons(cycles)
     @classmethod
     def get_polygon_from_cycle(self, cycle):
         cycle = cycle + [cycle[0]]
@@ -22,20 +26,37 @@ class Polygons():
     def get_coords_of_polygon(self, polygon):
         return list(map( self.qbit_coord_dict.get, polygon))
     @staticmethod
-    def distance(xy):
-        squared_diff = np.square(np.subtract(*xy))
-        return np.sqrt(squared_diff.sum())
-    @classmethod
-    def polygon_scope(self, polygon_coord):
-        polygon = self.get_polygon_from_cycle(polygon_coord)
-        return sum(list(map(self.distance, polygon)))
+    def polygon_length(polygon_coord):
+        return Polygon(polygon_coord).length
     @staticmethod
     def polygon_area(polygon_coord):
         return Polygon(polygon_coord).area
     def get_all_polygons(self, cycles):
         return list(map(self.get_polygon_from_cycle, cycles))
-    def get_all_polyg_coords(self, polygons):
-        return list(map(self.get_coords_of_polygon, polygons))
+    def get_all_polyg_coords(self):
+        return list(map(self.get_coords_of_polygon, self.polygons))
+    @staticmethod
+    def is_unit_square(polygon_coord):
+        return (Polygons.polygon_area(polygon_coord) == 1 
+                and Polygons.polygon_length(polygon_coord) == 4)
+    @staticmethod
+    def is_unit_triangle(polygon_coord):
+        return (Polygons.polygon_area(polygon_coord) == 0.5 
+                and int((Polygons.polygon_length(polygon_coord) - 2) * 1e4) == 14142)
+    def convex_hull(self):
+        qbit_coords = list(self.qbit_coord_dict.values())
+        pqbits = MultiPoint(qbit_coords)
+        envelop_polygon = pqbits.convex_hull
+        return list(envelop_polygon.exterior.coords)
+    @staticmethod
+    def get_grid_of_unit_squares(grid_length):
+        """from 37041377 stackoverflow"""
+        x = y = np.arange(grid_length)
+        hlines = [((x1, yi), (x2, yi)) for x1, x2 in zip(x[:-1], x[1:]) for yi in y]
+        vlines = [((xi, y1), (xi, y2)) for y1, y2 in zip(y[:-1], y[1:]) for xi in x]
+        grid = list(polygonize(MultiLineString(hlines + vlines)))
+        unit_square_coords = [list(square.exterior.coords) for square in grid]
+        return unit_square_coords
     def visualize(self, ax, polygon_coords):
         x, y = np.meshgrid(np.arange(self.K), np.arange(self.K))
         ax.scatter(x.flatten(), y.flatten(), color='grey', s=0.6)
@@ -48,6 +69,6 @@ class Polygons():
 
         for qbit, coord in self.qbit_coord_dict.items():
             ax.annotate(str(qbit), coord)
-            ax.scatter(*coord, color='black')
+            ax.scatter(*coord, color='red')
 
         return ax
