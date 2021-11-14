c05a7c4f4c57d9323c566400154a1deb9ea82dc0
Kerem <akd.kerem@gmail.com>
2021-11-15 00:54:47 +0100

    this file should include the search algos to find optimal layout, we start with MonteCarlo

---
+++ search.py
@@ -0,0 +1,15 @@
+class MC():
+
+    def __init__(self):
+        pass # graph, polygonobject
+
+    def change_single_qbit_coord(self):
+        pass
+    def step(self):
+        pass
+    # TODO: generiere graph, dann polygon object
+    # ändere qbit coordinate, berechne davon energie neu
+    # akzeptiere neue coordinate oder nicht
+    # kann man das theretisch so machen dass wenn man nur ein qbit ändert, nur die entsprechenden polygone geändetr werden müssen
+
+        
