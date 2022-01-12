64aba355e9f598cf98240f1e048a4965be3bb08a
Kerem <akd.kerem@gmail.com>
2022-01-12 11:46:14 +0100

    fix bug: some triangles have been blue

--- polygons.py
+++ polygons.py
@@ -247,9 +247,9 @@ class Polygons():
         ax.set_xticklabels([])
         for polygon in polygon_coords:
             fill, facecolor = False, None
-            if self.is_unit_square(polygon) == 0:
+            if len(polygon) == 4 and self.is_unit_square(polygon) == 0:
                 fill, facecolor = True, 'blue'
-            if self.is_unit_triangle(polygon) == 0:
+            if len(polygon) == 3 and self.is_unit_triangle(polygon) == 0:
                 fill, facecolor = True, 'red'
             patch = plt.Polygon(polygon,
                     zorder=0,
