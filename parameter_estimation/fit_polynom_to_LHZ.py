2713f8dca9bd1fc76803f0d1feef27e1eb3e61f4
Kerem <akd.kerem@gmail.com>
2022-01-14 12:16:56 +0100

    pep 8 and code cleaning

--- parameter_estimation/fit_polynom_to_LHZ.py
+++ parameter_estimation/fit_polynom_to_LHZ.py
@@ -1,10 +1,11 @@
 import numpy as np
 from CompilerQC import Polygons
-from scipy.special import binom 
+from scipy.special import binom
 import itertools
 import os.path
-homedir=os.path.expanduser("~/UniInnsbruck/")
-pathset=os.path.join(homedir,"CompilerQC/parameter_estimation/parameters")
+
+homedir = os.path.expanduser("~/UniInnsbruck/")
+pathset = os.path.join(homedir, "CompilerQC/parameter_estimation/parameters")
 
 # this function calculates the energy of LHZ analytically
 def scope_of_shapes(x, y, n):
@@ -15,21 +16,30 @@ def scope_of_shapes(x, y, n):
     ...
 
     calculate the scope of the rectengular, the "triangular" and the "cross"
-    maybe I will add a sketch 
+    maybe I will add a sketch
     """
-    n = n-2
+    n = n - 2
     square = 2 * x + 2 * y
     triangular = (
-      n - (y - 1)
-    + n - (x - 1)
-    + np.sqrt(y ** 2 + (n - (y - 1) - x) ** 2)
-    + np.sqrt(x ** 2 + (n - (x - 1) - y) ** 2))
+        n
+        - (y - 1)
+        + n
+        - (x - 1)
+        + np.sqrt(y ** 2 + (n - (y - 1) - x) ** 2)
+        + np.sqrt(x ** 2 + (n - (x - 1) - y) ** 2)
+    )
     cross = (
-      n - (y - 1) - x
-    + n - (x - 1) - y
-    + np.sqrt(y ** 2 + (n - (y - 1)) ** 2)
-    + np.sqrt(x ** 2 + (n - (x - 1)) ** 2))
+        n
+        - (y - 1)
+        - x
+        + n
+        - (x - 1)
+        - y
+        + np.sqrt(y ** 2 + (n - (y - 1)) ** 2)
+        + np.sqrt(x ** 2 + (n - (x - 1)) ** 2)
+    )
     return sum([triangular, square, cross]) - 3 * 4
+
 
 def total_energy_LHZ4(N_):
     """
@@ -37,12 +47,21 @@ def total_energy_LHZ4(N_):
     calculate the scope_of_shapes() for each (xy) and sum it
     """
     summe = 0
-    for N in range(4,N_ + 1):
-        summe += (sum(map(lambda x: scope_of_shapes(*x),
-                          [(i, j, n) for n in range (4, N + 1)
-                           for i in range(1, n) for j in range(1, n)
-                           if i + j <= n - 2])))
+    for N in range(4, N_ + 1):
+        summe += sum(
+            map(
+                lambda x: scope_of_shapes(*x),
+                [
+                    (i, j, n)
+                    for n in range(4, N + 1)
+                    for i in range(1, n)
+                    for j in range(1, n)
+                    if i + j <= n - 2
+                ],
+            )
+        )
     return summe
+
 
 def number_4plaqs(N):
     """
@@ -54,14 +73,16 @@ def number_4plaqs(N):
 
 def total_energy_LHZ3(N):
     """
-    calculates the scope (minus unit triangle scope) of 3er polygons in LHZ 
+    calculates the scope (minus unit triangle scope) of 3er polygons in LHZ
     and sums over it
     """
     polys = []
     for l in itertools.combinations([i for i in range(0, N)], 3):
-         polys.append(Polygons.is_unit_triangle(
-             Polygons.get_polygon_from_cycle(list(l))))
+        polys.append(
+            Polygons.is_unit_triangle(Polygons.get_polygon_from_cycle(list(l)))
+        )
     return sum(polys)
+
 
 def number_3plaqs(N):
     """
@@ -70,16 +91,18 @@ def number_3plaqs(N):
     C = N - 2
     return int(C)
 
+
 def fit_polynom_to_LHZ4(Ns, max_N=20):
     """
     FIT order 5 may be seen by analytical formula,
     or by the fact that they have to scale with
     the number of 4plaqs in LHZ
     """
-    Ns_for_fit = np.array([i for i in range(4,max_N)])
+    Ns_for_fit = np.array([i for i in range(4, max_N)])
     energy_LHZ4_for_fit = [total_energy_LHZ4(N) for N in Ns_for_fit]
     poly_LHZ4 = np.poly1d(np.polyfit(np.array(Ns_for_fit), energy_LHZ4_for_fit, 5))
     return poly_LHZ4(Ns)
+
 
 def scale_4plaqs(Ns, max_N=20):
     """
@@ -90,15 +113,17 @@ def scale_4plaqs(Ns, max_N=20):
     factors_poly_LHZ4_and_number_4plaqs = poly_LHZ4 / list(map(number_4plaqs, Ns))
     return factors_poly_LHZ4_and_number_4plaqs
 
+
 def fit_polynom_to_LHZ3(Ns, max_N=20):
     """
     order 4 due to the fact that they have to scale with the
     number of 3plaqs in LHZ
     """
-    Ns_for_fit = np.array([i for i in range(4,max_N)])
+    Ns_for_fit = np.array([i for i in range(4, max_N)])
     energy_LHZ3_for_fit = [total_energy_LHZ3(N) for N in Ns_for_fit]
     poly_LHZ3 = np.poly1d(np.polyfit(Ns_for_fit, energy_LHZ3_for_fit, 4))
     return poly_LHZ3(Ns)
+
 
 def scale_3plaqs(Ns, max_N=20):
     """
@@ -109,26 +134,34 @@ def scale_3plaqs(Ns, max_N=20):
     factors_poly_LHZ3_and_number_3plaqs = poly_LHZ3 / list(map(number_3plaqs, Ns))
     return factors_poly_LHZ3_and_number_3plaqs
 
+
 def save_polynom_coeffs():
     """
-    saves the coeffs of the fitted polynoms (fitted to the energy of the 
+    saves the coeffs of the fitted polynoms (fitted to the energy of the
     not satisfied plaqs in LHZ)
     """
     poly_LHZ3 = fit_polynom_to_LHZ3(Ns)
     poly_LHZ4 = fit_polynom_to_LHZ4(Ns)
-    np.save('polynomial_coeffs_LHZ4', poly_LHZ4)
-    np.save('polynomial_coeffs_LHZ3', poly_LHZ3)
-    
+    np.save("polynomial_coeffs_LHZ4", poly_LHZ4)
+    np.save("polynomial_coeffs_LHZ3", poly_LHZ3)
+
+
 def save_scaling_factors(max_N=20):
     """
     saves the factors of scale_4plaqs and scale_3plaqs
     """
-    Ns = np.array([i for i in range(4,max_N)])
+    Ns = np.array([i for i in range(4, max_N)])
     factors_poly_LHZ4_and_number_4plaqs = scale_4plaqs(Ns, max_N)
     factors_poly_LHZ3_and_number_3plaqs = scale_3plaqs(Ns, max_N)
-    np.save(os.path.join(pathset,"scaling_factors_LHZ4.npy"), factors_poly_LHZ4_and_number_4plaqs)
-    np.save(os.path.join(pathset,"scaling_factors_LHZ3.npy"), factors_poly_LHZ3_and_number_3plaqs)
+    np.save(
+        os.path.join(pathset, "scaling_factors_LHZ4.npy"),
+        factors_poly_LHZ4_and_number_4plaqs,
+    )
+    np.save(
+        os.path.join(pathset, "scaling_factors_LHZ3.npy"),
+        factors_poly_LHZ3_and_number_3plaqs,
+    )
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     save_scaling_factors()
-
--- polygons.py
+++ polygons.py
@@ -6,12 +6,13 @@ from shapely.ops import polygonize
 from shapely import wkt
 from CompilerQC import Graph
 
-class Polygons():
 
-    def __init__(self,
-                 logical_graph: Graph,
-                 core_bipartite_sets: list=[[],[]],
-                 ):
+class Polygons:
+    def __init__(
+        self,
+        logical_graph: Graph,
+        core_bipartite_sets: list = [[], []],
+    ):
         """
         logical_graph: logical graph with N nodes and K edges
         """
@@ -19,8 +20,7 @@ class Polygons():
         self.N = logical_graph.N
         self.K = logical_graph.K
         self.C = logical_graph.C
-        cycles = (logical_graph.get_cycles(3)
-                  + logical_graph.get_cycles(4))
+        cycles = logical_graph.get_cycles(3) + logical_graph.get_cycles(4)
         self.polygons = self.get_all_polygons(cycles)
         self.qbit_coord_dict = self.init_coords_for_qbits()
         # define qbits and their coords for core and outside core
@@ -39,9 +39,9 @@ class Polygons():
 
     @staticmethod
     def core_qbits_and_coords_from_sets(
-            U: list,
-            V: list,
-            ):
+        U: list,
+        V: list,
+    ):
         """
         input: two sets of logical bits, generating the
         complete bipartite graph
@@ -56,13 +56,13 @@ class Polygons():
         qbits = list(zip(lbit1.flatten(), lbit2.flatten()))
         sorted_qbits = list(map(tuple, map(sorted, qbits)))
         return dict(zip(sorted_qbits, coords))
-    
-    # TODO: if qbit not in qbits, raise error   
+
+    # TODO: if qbit not in qbits, raise error. move_to_coord should be better choosen than max
     def update_qbits_coords(
-            self,
-            qbits: list,
-            new_coords: list,
-            ):
+        self,
+        qbits: list,
+        new_coords: list,
+    ):
         """
         qbits: list of tuples
         new_coords: list of tuples
@@ -70,20 +70,16 @@ class Polygons():
         this qbit is moved to the max coord + (1, 0)
         """
         for qbit, new_coord in zip(qbits, new_coords):
-            qbit_coords =  self.qbit_coord_dict.values()
+            qbit_coords = self.qbit_coord_dict.values()
             if new_coord in qbit_coords:
-                qbit_to_move = dict(zip(
-                    self.qbit_coord_dict.values(),
-                    self.qbit_coord_dict.keys())
-                                   )[new_coord]
-                move_to_coord = tuple(
-                       np.add(max(qbit_coords), (1, 0)))
+                qbit_to_move = dict(
+                    zip(self.qbit_coord_dict.values(), self.qbit_coord_dict.keys())
+                )[new_coord]
+                move_to_coord = tuple(np.add(max(qbit_coords), (1, 0)))
                 self.qbit_coord_dict[qbit_to_move] = move_to_coord
-                self.update_core_and_movable_coords(
-                    qbit_to_move, move_to_coord)
+                self.update_core_and_movable_coords(qbit_to_move, move_to_coord)
             self.qbit_coord_dict[qbit] = new_coord
             self.update_core_and_movable_coords(qbit, new_coord)
-
 
     def update_core_and_movable_coords(
         self,
@@ -94,67 +90,56 @@ class Polygons():
         update core_coords or movable_coords
         """
         if qbit in self.core_qbits:
-            self.core_coords[
-                self.core_qbits.index(
-                    qbit)] = new_coord
+            self.core_coords[self.core_qbits.index(qbit)] = new_coord
         elif qbit in self.movable_qbits:
-            self.movable_coords[
-                self.movable_qbits.index(
-                    qbit)] = new_coord
+            self.movable_coords[self.movable_qbits.index(qbit)] = new_coord
         else:
-            raise AssertionError("qbit is neither in the core nor in the movable part", qbit)
+            raise AssertionError(
+                "qbit is neither in the core nor in the movable part", qbit
+            )
+
     @classmethod
-    def get_polygon_from_cycle(
-            self,
-            cycle: list
-            ):
+    def get_polygon_from_cycle(self, cycle: list):
         """
         cycle: cycle in graph
         """
         cycle = cycle + [cycle[0]]
-        polygon = list(map(tuple, 
-            map(sorted, zip(cycle, cycle[1:]))))
+        polygon = list(map(tuple, map(sorted, zip(cycle, cycle[1:]))))
         return polygon
-
 
     def init_coords_for_qbits(self):
         """
-        initialization of qbits with random coordinates 
+        initialization of qbits with random coordinates
         return: dictionary with qbits and coordinates
         """
         coords = list(np.ndindex(self.K, self.K))
         np.random.shuffle(coords)
-        return dict(zip(self.qbits, coords[:self.K]))
-
+        return dict(zip(self.qbits, coords[: self.K]))
 
     def get_coords_of_polygon(self, polygon):
         """
         return: list of coords of single polygon
         """
-        return list(map( self.qbit_coord_dict.get, polygon))
-
+        return list(map(self.qbit_coord_dict.get, polygon))
 
     @staticmethod
     def polygon_length(polygon_coord):
         return Polygon(polygon_coord).length
 
-
     @staticmethod
     def polygon_area(polygon_coord):
         return Polygon(polygon_coord).area
 
-
     def get_all_polygons(self, cycles):
         return list(map(self.get_polygon_from_cycle, cycles))
 
-
     def get_all_polygon_coords(self):
         """
-        return: list of lists, each list contains 
+        return: list of lists, each list contains
         coordinates of a polygon
         """
         return list(map(self.get_coords_of_polygon, self.polygons))
-            
+
     # TODO: this function can get very important, think more about it
     def polygon_weights(self, closeness_to_plaquette):
         ctp = np.array(closeness_to_plaquette)
@@ -163,17 +148,17 @@ class Polygons():
         normalized_weights = weights / (weights.sum() / self.C)
         return normalized_weights
 
-
     @staticmethod
     def is_unit_square(polygon_coord):
         """
         measure closeness to plaquette
         """
-        scope = [LineString([*x]).length
-                for x in itertools.combinations(
-                    polygon_coord, 2)]
+        scope = [
+            LineString([*x]).length for x in itertools.combinations(polygon_coord, 2)
+        ]
         return sum(scope) - (4 + 2 * np.sqrt(2))
-#        return Polygons.polygon_length(polygon_coord) - 4
+
+    #        return Polygons.polygon_length(polygon_coord) - 4
 
     @staticmethod
     def is_unit_triangle(polygon_coord):
@@ -182,44 +167,47 @@ class Polygons():
         """
         return Polygons.polygon_length(polygon_coord) - (2 + np.sqrt(2))
 
-
     @staticmethod
     def neighbours(coords: tuple):
         x, y = coords
-        return [(x, y+1), (x+1, y+1), (x+1, y), (x+1, y-1), (x, y-1), (x-1, y-1), (x-1, y), (x-1, y+1)]
+        return [
+            (x, y + 1),
+            (x + 1, y + 1),
+            (x + 1, y),
+            (x + 1, y - 1),
+            (x, y - 1),
+            (x - 1, y - 1),
+            (x - 1, y),
+            (x - 1, y + 1),
+        ]
 
     def inside_core_coords(self):
         """
-        return: list of coords which are inside the core, 
+        return: list of coords which are inside the core,
         not at the edge of it
         """
         neighbour_coords = map(Polygons.neighbours, self.core_coords)
         is_inside_core = list(
-                map(lambda s: set(s).issubset(self.core_coords),
-                    neighbour_coords)
-                )
-        inside_coords = list(
-                itertools.compress(self.core_coords, is_inside_core)
-                )
+            map(lambda s: set(s).issubset(self.core_coords), neighbour_coords)
+        )
+        inside_coords = list(itertools.compress(self.core_coords, is_inside_core))
         return inside_coords
-    
+
     def polygons_outside_core(self):
         """
-        returns all polygons which are not connected to 
+        returns all polygons which are not connected to
         coords inside the core
         """
         inside_coords = self.inside_core_coords()
-        return [coords for coords in self.get_all_polygon_coords()
-         if set(coords).isdisjoint(inside_coords)]
-            
+        return [
+            coords
+            for coords in self.get_all_polygon_coords()
+            if set(coords).isdisjoint(inside_coords)
+        ]
 
-
-    def convex_hull(
-            self,
-            qbit_coords: list=None
-            ):
+    def convex_hull(self, qbit_coords: list = None):
         """
-        return envelop polygon of qbit_coords, if 
+        return envelop polygon of qbit_coords, if
         qbit_coords are None, all qbits coords are considered
         """
         if qbit_coords is None:
@@ -228,23 +216,21 @@ class Polygons():
         envelop_polygon = qbits.convex_hull
         return list(envelop_polygon.exterior.coords)
 
-
     def center_of_convex_hull(
-            self,
-            qbit_coords: list=None,
-            ):
+        self,
+        qbit_coords: list = None,
+    ):
         """
         return center coords of enevelop of qbit_coords,
         if None, all qbits are considered
         """
         center = Polygon(self.convex_hull(qbit_coords)).centroid
-        return (center.x, center.y) 
-
+        return (center.x, center.y)
 
     def qbit_distances_to_center(
-            self,
-            qbit_coords: list=None,
-            ):
+        self,
+        qbit_coords: list = None,
+    ):
         """
         list of distances for each qbit to center of qbit_coords,
         could e.g be the core_coords --> core_center
@@ -255,7 +241,6 @@ class Polygons():
         distances = list(map(lambda x: Point(center).distance(Point(x)), qbit_coords))
         return distances
 
-    
     def move_center_to_middle(self):
         """
         calculates the CoM of the convex hull
@@ -264,11 +249,11 @@ class Polygons():
         center_x, center_y = map(int, self.center_of_convex_hull())
         middle_x = middle_y = self.K // 2
         move_x, move_y = center_x - middle_x, center_y - middle_y
-        new_coords = [(coord[0] - move_x, coord[1] - move_y)
-                for coord in self.qbit_coord_dict.values()]
+        new_coords = [
+            (coord[0] - move_x, coord[1] - move_y)
+            for coord in self.qbit_coord_dict.values()
+        ]
         self.update_qbits_coords(self.qbits, new_coords)
-
-        
 
     @staticmethod
     def get_grid_of_unit_squares(grid_length):
@@ -283,31 +268,30 @@ class Polygons():
         unit_square_coords = [list(square.exterior.coords) for square in grid]
         return unit_square_coords
 
-
     def visualize(self, ax, polygon_coords, zoom=1):
         x, y = np.meshgrid(np.arange(self.K), np.arange(self.K))
-        ax.scatter(x.flatten(), y.flatten(), color='grey', s=0.6)
+        ax.scatter(x.flatten(), y.flatten(), color="grey", s=0.6)
         ax.set_yticklabels([])
         ax.set_xticklabels([])
         for polygon in polygon_coords:
             fill, facecolor = False, None
             if len(polygon) == 4 and self.is_unit_square(polygon) == 0:
-                fill, facecolor = True, 'blue'
+                fill, facecolor = True, "lightblue"
             if len(polygon) == 3 and self.is_unit_triangle(polygon) == 0:
-                fill, facecolor = True, 'red'
-            patch = plt.Polygon(polygon,
-                    zorder=0,
-                    fill=fill,
-                    lw=0,
-                    facecolor = facecolor)
+                fill, facecolor = True, "indianred"
+            patch = plt.Polygon(polygon, zorder=0, fill=fill, lw=0, facecolor=facecolor)
             ax.add_patch(patch)
         for qbit, coord in self.qbit_coord_dict.items():
             ax.annotate(str(qbit), coord)
-            ax.scatter(*coord, color='red')
+            ax.scatter(*coord, color="red")
         x_range = sorted(self.convex_hull(), key=lambda x: x[0])
         y_range = sorted(self.convex_hull(), key=lambda y: y[1])
-        ax.set_xlim(min(x_range[0][0], y_range[0][1])-zoom,
-                  max(x_range[-1][0], y_range[-1][1])+zoom)
-        ax.set_ylim(min(x_range[0][0], y_range[0][1])-zoom,
-                  max(x_range[-1][0], y_range[-1][1])+zoom)
+        ax.set_xlim(
+            min(x_range[0][0], y_range[0][1]) - zoom,
+            max(x_range[-1][0], y_range[-1][1]) + zoom,
+        )
+        ax.set_ylim(
+            min(x_range[0][0], y_range[0][1]) - zoom,
+            max(x_range[-1][0], y_range[-1][1]) + zoom,
+        )
         return ax
