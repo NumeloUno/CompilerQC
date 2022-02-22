60cfb40187267cb2f1117918cd799a4afd1556ca
Kerem <akd.kerem@gmail.com>
2022-02-22 18:08:43 +0100

    generate lhz samples, by function and not onöy during a foor loop

--- reversed_engineering/generate_lhz_samples.py
+++ reversed_engineering/generate_lhz_samples.py
@@ -7,8 +7,12 @@ from uuid import uuid4
 import os
 from tqdm import tqdm
 
-for N in tqdm(range(4, 18), desc="Generate lhz samples"):
-
+def generate_LHZ_problem(N: int):
+    """
+    return the adj matrix for a complete connected graph 
+    of size N and its compiled solution for the 
+    physical graph as qbit coord dictionary
+    """
     graph = Graph.complete(N)
     polygon_object = Polygons(graph)
     lhz_coords = qbits = polygon_object.qbits
@@ -19,13 +23,18 @@ for N in tqdm(range(4, 18), desc="Generate lhz samples"):
     C = polygon_object.C
     K = len(qbits)
     N = np.abs(C - K - 1)
+    return adj_matrix, qbit_coord_dict
 
-    eventid = datetime.now().strftime("%Y%m-%d%H-%M%S-") + str(uuid4())
-    # Save
-    save_in_folder = "lhz"
-    os.makedirs(paths.database_path / save_in_folder / f"problem_N_{N}_K_{K}_C_{C}", exist_ok=True)
-    dictionary = {"qbit_coord_dict": qbit_coord_dict, "graph_adj_matrix": adj_matrix}
-    np.save(
-        paths.database_path / save_in_folder / f"problem_N_{N}_K_{K}_C_{C}" / f"LHZ_N_{N}.npy",
-        dictionary,
-    )
+if __name__ == "__main__":
+    
+    for N in tqdm(range(4, 18), desc="Generate lhz samples"):
+        adj_matrix, qbit_coord_dict = generate_LHZ_problem(N)
+        eventid = datetime.now().strftime("%Y%m-%d%H-%M%S-") + str(uuid4())
+        # Save
+        save_in_folder = "lhz"
+        os.makedirs(paths.database_path / save_in_folder / f"problem_N_{N}_K_{K}_C_{C}", exist_ok=True)
+        dictionary = {"qbit_coord_dict": qbit_coord_dict, "graph_adj_matrix": adj_matrix}
+        np.save(
+            paths.database_path / save_in_folder / f"problem_N_{N}_K_{K}_C_{C}" / f"LHZ_N_{N}.npy",
+            dictionary,
+        )
