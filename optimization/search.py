f71009cc9a8ef53874e48371066273c2e2e2b69e
Kerem Akdogan <csba1344@fermion.uibk.ac.at>
2022-03-30 18:20:52 +0200

    benchmarks for temperature und polygons weights

--- optimization/search.py
+++ optimization/search.py
@@ -9,6 +9,7 @@ Simulated Annealing and Boltzmann Machines,
 size of neighbourhood approx bei K
 """
 
+
 class MC:
     """
     Find optimal configuration of qbits given
@@ -20,6 +21,7 @@ class MC:
         energy_object: Energy,
         delta: float = 1,
         chi_0: float = 0.8,
+        alpha: float = 0.8,
         repetition_rate: int = None,
         recording: bool = False,
     ):
@@ -27,22 +29,28 @@ class MC:
         repetition rate: how many moves with constant temperature
         delta: how large are the jumps in temperature change
         T_0: initial temperture, can be later set with initial_temperature()
+        alpha: decreasing paramter for kirkpatrick temperature schedule
         """
-        #self.prof = cProfile.Profile()
+        # self.prof = cProfile.Profile()
 
         self.energy = energy_object
         self.n_total_steps = 0
         self.chi_0 = chi_0
         self.delta = delta
+        self.alpha = alpha
         self.temperature_kirkpatrick = False
+        self.temperature_C = False
+        self.temperature_linearC = False
         self.repetition_rate = repetition_rate
         if self.repetition_rate is None:
             self.repetition_rate = self.energy.polygon_object.qbits.graph.K
         # compute total energy once at the beginning
-        self.total_energy, self.number_of_plaquettes = self.energy(self.energy.polygon_object.qbits)
+        self.total_energy, self.number_of_plaquettes = self.energy(
+            self.energy.polygon_object.qbits
+        )
 
         self.recording = recording
-        if self.recording: 
+        if self.recording:
             self.record_temperature = []
             self.record_total_energy = []
             self.record_mean_energy = []
@@ -85,7 +93,7 @@ class MC:
         if type(coord_or_qbit) == tuple:
             qbit.coord = self.old_coord
         else:
-            assert self.old_coord == None, 'something went wrong'
+            assert self.old_coord == None, "something went wrong"
             self.energy.polygon_object.qbits.swap_qbits(coord_or_qbit, qbit)
 
     def generate_coord(self):
@@ -96,11 +104,13 @@ class MC:
         """
         if self.n_total_steps % self.repetition_rate == 0:
             possible_coords = self.energy.polygon_object.envelop_rect()
-            self.possible_coords = list(set(possible_coords) 
-                                        - set(self.energy.polygon_object.qbits.core_qbit_coords))
+            self.possible_coords = list(
+                set(possible_coords)
+                - set(self.energy.polygon_object.qbits.core_qbit_coords)
+            )
         return random.choice(self.possible_coords)
 
-    def step(self, operation: str=None):
+    def step(self, operation: str = None):
         """
         move qbit or swap qbits and return energy of old and new configuration
         to expand this function for g(x) > 1, select_move() shouls use random.choices(k>1)
@@ -108,16 +118,17 @@ class MC:
         self.qbits_to_move = self.select_move()
         current_energy, current_n_plaqs = self.energy(self.qbits_to_move)
         self.apply_move(*self.qbits_to_move)
-        new_energy, new_n_plaqs= self.energy(self.qbits_to_move)
+        new_energy, new_n_plaqs = self.energy(self.qbits_to_move)
         self.current_number_of_plaquettes = self.number_of_plaquettes
-        self.number_of_plaquettes += (new_n_plaqs - current_n_plaqs)
+        self.number_of_plaquettes += new_n_plaqs - current_n_plaqs
         self.n_total_steps += 1
         return current_energy, new_energy
 
     def metropolis(self, current_energy, new_energy):
         temperature = self.temperature
         delta_energy = new_energy - current_energy
-        if self.recording: self.record_temperature.append(temperature)
+        if self.recording:
+            self.record_temperature.append(temperature)
         if delta_energy < 0:
             self.total_energy += delta_energy
             if self.recording:
@@ -125,24 +136,25 @@ class MC:
                 self.record_acc_probability.append(1)
             return
         acceptance_probability = np.exp(-delta_energy / temperature)
-        if self.recording: self.record_acc_probability.append(acceptance_probability)        
+        if self.recording:
+            self.record_acc_probability.append(acceptance_probability)
         if random.random() < acceptance_probability:
             self.total_energy += delta_energy
-            if self.recording: self.record_bad_moves.append(self.n_total_steps)
+            if self.recording:
+                self.record_bad_moves.append(self.n_total_steps)
             return
         else:
             self.reverse_move(*self.qbits_to_move)
             self.number_of_plaquettes = self.current_number_of_plaquettes
-            if self.recording: self.record_rejected_moves.append(self.n_total_steps)
+            if self.recording:
+                self.record_rejected_moves.append(self.n_total_steps)
 
     def apply(
-        self,
-        operation,
-        n_steps,
+        self, operation, n_steps,
     ):
-        
-        #self.prof.enable()
-        
+
+        # self.prof.enable()
+
         C = self.energy.polygon_object.graph.C
         for i in range(n_steps):
             if self.number_of_plaquettes == C:
@@ -150,23 +162,19 @@ class MC:
             self.update_mean_and_variance()
             current_energy, new_energy = self.step(operation)
             self.metropolis(current_energy, new_energy)
-            
-        #self.prof.disable()
+
+        # self.prof.disable()
 
         return new_energy - current_energy
-    
-    
-    def optimization_schedule(
-        self,
-    ):
+
+    def optimization_schedule(self,):
         """
         schedule is a dict = {operation:n_steps}
         set schedule for the optimization
         """
         for operation, n_steps in self.operation_schedule.items():
             self.apply(operation, n_steps)
-            
-            
+
     def initial_temperature(self, size_of_S=1):
         """
         estimate the initial Temperature T_0,
@@ -189,24 +197,48 @@ class MC:
         after n_total_steps > 1,
         since welford needs it like this
         """
+        if self.temperature_linearC:
+            C = self.energy.polygon_object.qbits.graph.C
+            p = self.number_of_plaquettes
+            self.current_temperature = self.rho * self.T_0 * (
+                    C - p) / C + 1e-2
+            return self.current_temperature
+        if self.temperature_C:
+            self.current_temperature = self.T_0 * np.exp(
+                - self.rho * self.number_of_plaquettes
+                / (
+                    self.energy.polygon_object.qbits.graph.C
+                    - self.number_of_plaquettes
+                    + 1e-2
+                )
+            ) + 1e-2
+            return self.current_temperature
+        if self.linear_in_moves:
+            self.current_temperature = self.T_0 * (self.n_moves - self.n_total_steps) / self.n_moves + 1e-2
+            return self.current_temperature
+
         if self.n_total_steps % self.repetition_rate == 0:
-            
             if self.temperature_kirkpatrick:
-                alpha = 0.8
-                new_temperature = alpha * self.current_temperature
-                self.current_temperature = new_temperature   
-            
+                new_temperature = self.alpha * self.current_temperature
+                self.current_temperature = new_temperature 
+                return self.current_temperature
+
+            sigmoid = lambda x: 1 - 1 / (1 + np.exp(-x))
+            if self.temperature_kirkpatrick_sigma:
+                new_temperature = self.alpha * self.current_temperature
+                self.current_temperature = new_temperature + sigmoid(self.variance_energy)
+                return self.current_temperature
             else:
                 new_temperature = self.current_temperature / (
                     1
                     + self.current_temperature
                     * np.log(1 + self.delta)
-                    /  (3 * np.sqrt(self.variance_energy) + 1e-3)
+                    / (3 * np.sqrt(self.variance_energy) + 1e-3)
                 )
                 self.current_temperature = new_temperature
-                
+
         return self.current_temperature
-    
+
     def update_mean_and_variance(self):
         """
         update mean and variance on the fly according to Welford algorithm on wikipedia
@@ -223,7 +255,7 @@ class MC:
             self.record_total_energy.append(self.total_energy)
             self.record_mean_energy.append(self.mean_energy)
             self.record_variance_energy.append(self.variance_energy)
-            
+
     def update_mean_variance(self, n, µ_m, s_m2, x_n):
         """
         update mean and variance on the fly according to Welford algorithm on wikipedia
@@ -233,7 +265,8 @@ class MC:
         s_n2 = s_m2 + (x_n - µ_m) ** 2 / (n + 1) - s_m2 / n
         return µ_n, s_n2
 
-from copy import deepcopy 
+
+from copy import deepcopy
 from CompilerQC import Qbits, Polygons, Energy
 
 
