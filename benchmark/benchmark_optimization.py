8baf7c35086ad747281beee8ee5ebb59b1c79459
Kerem Akdogan <csba1344@fermion.uibk.ac.at>
2022-03-30 18:20:52 +0200

    benchmarks for temperature und polygons weights

--- benchmark/benchmark_optimization.py
+++ benchmark/benchmark_optimization.py
@@ -1,52 +1,60 @@
-from functions_for_benchmarking import update_mc, check_update, evaluate_optimization, run_benchmark, head
+from functions_for_benchmarking import update_mc, evaluate_optimization, run_benchmark, head
+from CompilerQC import functions_for_database as dataset
 from tqdm import tqdm
 from CompilerQC import Graph, Qbits, Polygons, Energy, MC, paths, core
 import numpy as np
 import pickle
+import argparse
+import os.path
 
 def open_file(args):
+    """
+    open txt where benchmark results will be saved,
+    if file does not exist yet, create it and append head
+    """
     print(head)
     path_to_results = (
-        paths.benchmark_results_path / f"mc_benchmark_results_{args.id_of_benchmark}.txt"
+        paths.benchmark_results_path / f"mc_benchmark_results_{args.id_of_benchmark}_{args.extra_id}.txt"
     )
-    file = open(path_to_results, "a")
-    file.write(f"with core {args.with_core} \n")
-    file.write(head)
+    if os.path.exists(path_to_results):
+        file = open(path_to_results, "a")
+        file.write(head)
+    else:
+        file = open(path_to_results, "a")
     return file
 
-# def benchmark_problem_folder_with_exact_scaling(args):
-#     """
-#     calculate the scaling for each 3er and 4er plaq such that 
-#     the total energy of the compiled solution is zero,
-#     if the same compiled solution is found as in the dataset
-#     """
-#     problems = get_files_to_problems(
-#                 problem_folder=args.problem_folder,
-#                 min_C=args.min_C,
-#                 max_C=args.max_C,
-#                 )
-#     file = open_file(args)
-#     for file_ in problems:
-#         # read graph and qubit to coord translation from file
-#         graph_adj_matrix, qubit_coord_dict = (
-#             problem_from_file(file_))
-#         # scopes for nonplaqs and plaqs
-#         polygon_scopes, NKC, n_cycles = energy_from_problem(graph_adj_matrix, qubit_coord_dict)
-#         n3, n4, p3, p4 = polygon_scopes
-#         graph = Graph(adj_matrix=graph_adj_matrix)
-#         # contribution of nonplaqs to each constraint 
-#         scaling_for_plaq3 = sum(n3) / len(p3)
-#         scaling_for_plaq4 = sum(n4) / len(p4)
-#         # initialise energy_object
-#         qbits = Qbits.init_qbits_from_dict(graph,dict())
-#         polygon_object = Polygons(qbits=qbits)
-#         energy = Energy(
-#             polygon_object,
-#             scaling_for_plaq3=scaling_for_plaq3,
-#             scaling_for_plaq4=scaling_for_plaq4,
-#         )
-#         file = run_benchmark(file, energy, args.batchsize, args.id_of_benchmark)
-#     file.close()
+def benchmark_problem_folder_with_exact_scaling(args):
+    """
+    calculate the scaling for each 3er and 4er plaq such that 
+    the total energy of the compiled solution is zero,
+    if the same compiled solution is found as in the dataset
+    """
+    problems = dataset.get_files_to_problems(
+                problem_folder=args.problem_folder,
+                min_C=args.min_C,
+                max_C=args.max_C,
+                )
+    file = open_file(args)
+    for file_ in problems:
+        # read graph and qubit to coord translation from file
+        graph_adj_matrix, qubit_coord_dict = (
+            dataset.problem_from_file(file_))
+        # scopes for nonplaqs and plaqs
+        polygon_scopes, NKC, n_cycles = dataset.energy_from_problem(graph_adj_matrix, qubit_coord_dict)
+        n3, n4, p3, p4 = polygon_scopes
+        graph = Graph(adj_matrix=graph_adj_matrix)
+        # contribution of nonplaqs to each constraint 
+        scaling_for_plaq3 = scaling_for_plaq4 = (sum(n4)+sum(n3)) / graph.C          
+        # initialise energy_object
+        qbits = Qbits.init_qbits_from_dict(graph, dict())
+        polygon_object = Polygons(qbits=qbits)
+        energy = Energy(
+            polygon_object,
+            scaling_for_plaq3=scaling_for_plaq3,
+            scaling_for_plaq4=scaling_for_plaq4,
+        )
+        file = run_benchmark(file, energy, args)
+    file.close()
     
 def benchmark_energy_scaling_by_yaml(args):
     """
@@ -55,7 +63,7 @@ def benchmark_energy_scaling_by_yaml(args):
     scale plaqs by value in the yaml
     """
     file = open_file(args)
-    for N in range(4, args.N +1):
+    for N in range(args.min_N, args.max_N +1):
         graph = Graph.complete(N)
         core_qubits, core_coords = [], []
         if args.with_core:
@@ -70,7 +78,7 @@ def benchmark_energy_scaling_by_yaml(args):
             qbits.assign_core_qbits(core_qubits)
         polygon_object = Polygons(qbits)
         energy = Energy(polygon_object)
-        file = run_benchmark(file, energy, args.batchsize, args.id_of_benchmark)
+        file = run_benchmark(file, energy, args)
     file.close()
        
 def benchmark_MLP_energy_scaling(args):
@@ -80,7 +88,7 @@ def benchmark_MLP_energy_scaling(args):
     scale plaqs by prediction of MLP
     """
     file = open_file(args)
-    for N in range(4, args.N +1):
+    for N in range(args.min_N, args.max_N +1):
         graph = Graph.complete(N)
         core_qubits, core_coords = [], []
         if args.with_core:
@@ -102,7 +110,7 @@ def benchmark_MLP_energy_scaling(args):
             scaling_for_plaq3=scaling_for_plaq3,
             scaling_for_plaq4=scaling_for_plaq4,
         )
-        file = run_benchmark(file, energy, args.batchsize, args.id_of_benchmark)
+        file = run_benchmark(file, energy, args)
     file.close()
     
 def benchmark_energy_scaling_by_max_C(args):
@@ -113,7 +121,7 @@ def benchmark_energy_scaling_by_max_C(args):
     to max_C:energy dataset
     """
     file = open_file(args)
-    for N in range(4, args.N +1):
+    for N in range(args.min_N, args.max_N +1):
         graph = Graph.complete(N)
         core_qubits, core_coords = [], []
         if args.with_core:
@@ -137,7 +145,42 @@ def benchmark_energy_scaling_by_max_C(args):
             scaling_for_plaq3=scaling_for_plaq3,
             scaling_for_plaq4=scaling_for_plaq4,
         )
-        file = run_benchmark(file, energy, args.batchsize, args.id_of_benchmark)
+        file = run_benchmark(file, energy, args)
+    file.close()
+    
+def benchmark_energy_scaling_by_LHZ_C(args):
+    """
+    benchmark the search of compiled graphs for 
+    fully connected logical graphs,
+    scale by prediction of polynom fittet
+    to LHZ solutions
+    """
+    file = open_file(args)
+    for N in range(args.min_N, args.max_N +1):
+        graph = Graph.complete(N)
+        core_qubits, core_coords = [], []
+        if args.with_core:
+            #nn = core.largest_complete_bipartite_graph(graph)
+            nn = (N // 2, N - (N // 2))
+            K_nn = core.complete_bipartite_graph(*nn)
+            U, V = core.parts_of_complete_bipartite_graph(graph.to_nx_graph(), K_nn)
+            core_qubits, core_coords = core.qbits_and_coords_of_core(U, V)
+
+        poly_coeffs = np.load(paths.energy_scalings / 'LHZ_energy_C_fit.npy')
+        poly_function = np.poly1d(poly_coeffs)
+        scaling_for_plaq3 = scaling_for_plaq4 = poly_function(graph.C) / graph.C  
+
+        # initialise energy_object
+        qbits = Qbits.init_qbits_from_dict(graph, dict(zip(core_qubits, core_coords)))
+        if args.with_core:
+            qbits.assign_core_qbits(core_qubits)
+        polygon_object = Polygons(qbits)
+        energy = Energy(
+            polygon_object,
+            scaling_for_plaq3=scaling_for_plaq3,
+            scaling_for_plaq4=scaling_for_plaq4,
+        )
+        file = run_benchmark(file, energy, args)
     file.close()
     
 if __name__ == "__main__":
@@ -154,7 +197,7 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "-b",
-        "--batchsize",
+        "--batch_size",
         type=int,
         default=100,
         help="how many times each schedule is evaluated default is 100",
@@ -162,7 +205,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "-id",
         "--id_of_benchmark",
-        type=int,
+        type=str,
         default="",
         help="save results using id (which is also in filename of mc_parameters.yaml) in filename",
     )
@@ -174,11 +217,18 @@ if __name__ == "__main__":
         help="set the minimum C, for which benchmarks should be done",
     )
     parser.add_argument(
-        "-N",
-        "--N",
+	"-maxN",
+        "--max_N",
         type=int,
-        default=1,
+        default=12,
         help="benchmark_LHZ up to N",
+    )
+    parser.add_argument(
+	"-minN",
+        "--min_N",
+        type=int,
+        default=4,
+        help="benchmark_LHZ from minimum N",
     )
     parser.add_argument(
         "-maxC",
@@ -194,6 +244,44 @@ if __name__ == "__main__":
         default=False,
         help="start with bipartite core",
     )
+    parser.add_argument(
+        "-ed",
+        "--extra_id",
+        type=str,
+        default="",
+        help="some extra name to add to result filename",
+    )
     args = parser.parse_args()
-    benchmark_problem_folder(args)
-
+    if args.id_of_benchmark == 'maxCpolynom':
+        benchmark_energy_scaling_by_max_C(args)
+    if args.id_of_benchmark == 'LHZCpolynom':
+        benchmark_energy_scaling_by_LHZ_C(args)
+    if args.id_of_benchmark == 'MLP':
+        benchmark_MLP_energy_scaling(args)
+    if args.id_of_benchmark == 'temp_schedule2':
+        args.extra_id = 'MLP'
+        benchmark_MLP_energy_scaling(args)
+    if args.id_of_benchmark == 'delta':
+        args.extra_id = 'MLP'
+        benchmark_MLP_energy_scaling(args)
+    if args.id_of_benchmark == 'chi_0':
+        args.extra_id = 'MLP'
+        benchmark_MLP_energy_scaling(args)    
+    if args.id_of_benchmark == 'energyscaling':
+        benchmark_energy_scaling_by_yaml(args)
+    if args.extra_id == 'exact_scaling':
+        benchmark_problem_folder_with_exact_scaling(args)
+    if args.id_of_benchmark == 'nmoves':
+        args.extra_id = 'MLP_linear_tempschedule_in_moves'
+        benchmark_MLP_energy_scaling(args)
+    if args.id_of_benchmark == 'temperature_C':
+        args.extra_id = 'MLP'
+        benchmark_MLP_energy_scaling(args)
+    if args.id_of_benchmark == 'temperature_linearC':
+        args.extra_id = 'MLP'
+        benchmark_MLP_energy_scaling(args)
+    if args.id_of_benchmark == 'temperature_kirkpatrick_sigma':
+        args.extra_id = 'MLP'
+        benchmark_MLP_energy_scaling(args)
+    else:
+        print("id not found in parameters folder")
--- benchmark/functions_for_benchmarking.py
+++ benchmark/functions_for_benchmarking.py
@@ -9,10 +9,10 @@ from copy import deepcopy
 from CompilerQC import Graph, Qbits, Polygons, Energy, MC, paths, core
 
 
-head = "{:9} {:12} {:3} {:3} {:6} {:6} {:16} {:15} {:18} {:15} {:15}\n".format(
+head = "{:9} {:12} {:3} {:3} {:6} {:6} {:16} {:15} {:18} {:15} {:15} {:6} \n".format(
     'name', 'succes_rate', 'N', 'C', 'chi_0', 'delta',
     'repetition_rate', 'n_total_steps', 'initial_temperature',
-    'scaling_plaq3', 'scaling_plaq4')
+    'scaling_plaq3', 'scaling_plaq4', 'with_core')
 
 def update_mc(mc, mc_schedule) -> MC:
     """
@@ -46,59 +46,51 @@ def evaluate_optimization(
         mc = update_mc(mc, mc_schedule)
         if mc.current_temperature == 0:
             initial_temperature = mc.initial_temperature()
+            mc.T_0 = initial_temperature
             mc.current_temperature = initial_temperature
+            mc.n_moves = mc_schedule['n_moves']
         for repetition in range(mc_schedule['n_moves']):
             mc.optimization_schedule()
         success_rate.append((mc.energy.polygon_object.qbits.graph.C
                              - mc.energy.polygon_object.number_of_plaqs
                             ) == 0)
-    return sum(success_rate) / batch_size, [mc.chi_0, mc.delta, mc.repetition_rate,
-                                            mc.n_total_steps, initial_temperature,
-                                            mc.energy.scaling_for_plaq3, mc.energy.scaling_for_plaq4]
+    return (sum(success_rate) / batch_size,
+            mc.energy.polygon_object.qbits.graph.N,
+            mc.energy.polygon_object.qbits.graph.C,
+            mc.chi_0, mc.delta, mc.repetition_rate,
+            mc.n_total_steps, initial_temperature,
+            mc.energy.scaling_for_plaq3,
+            mc.energy.scaling_for_plaq4)
             
 def run_benchmark(
     file,
     energy: Energy,
-    batch_size: int = 100,
-    id_of_benchmark: str = "",
+    args,
 ):
     """
     evaluate simulated annealing schedules from mc_parameters yaml
     and save success rate in corresponding txt
+    args contain args.batch_size, args.id_of_benchmarks
     """
-    path_to_config = paths.parameters_path / f"mc_parameters_{id_of_benchmark}.yaml"
-#     path_to_results = (
-#         paths.benchmark_results_path / f"mc_benchmark_results_{id_of_benchmark}.txt"
-#     )
+    path_to_config = paths.parameters_path / f"mc_parameters_{args.id_of_benchmark}.yaml"
     with open(path_to_config) as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
     # evaluate schedule from yaml and save it
-#     file = open(path_to_results, "a")
-#     file.write(head)
     for name, mc_schedule in config.items():
-#         check_update(energy, mc_schedule)
-        succes_rate,  [chi_0, delta, repetition_rate,
-                       n_total_steps, initial_temperature,
-                       scaling_plaq3, scaling_plaq4] = (
-            evaluate_optimization(energy, mc_schedule, batch_size)
-        )
-        N, C = energy.polygon_object.qbits.graph.N, energy.polygon_object.qbits.graph.C
-        result = "{:15} {:4} {:4} {:4} {:4} {:6} {:4} {:9} {:8.0f} {:8.0f} {:8.0f}\n".format(
-            name, str(succes_rate), str(N), str(C), str(chi_0), str(delta),
-            str(repetition_rate), str(n_total_steps), initial_temperature,
-            scaling_plaq3, scaling_plaq4)
+        write_to_file = evaluate_optimization(energy, mc_schedule, args.batch_size)
+        result = "{:15} {:4} {:4} {:4} {:4} {:6} {:4} {:9} {:8.0f} {:8.0f} {:8.0f} {:6} \n".format(
+            name, *write_to_file, args.with_core)
         file.write(result)
         print(result)
-    return file #file.close()
-
+    return file 
     
-def check_update(energy: Energy, mc_schedule: dict):
-    """check if upate from yaml is working"""
-    mc = MC(deepcopy(energy))
-    mc = update_mc(mc, mc_schedule)
-    if 'energy_scaling' in list(mc_schedule.keys()):
-        assert mc.energy.scaling_for_plaq3 ==  mc_schedule['energy_scaling']['scaling_for_plaq3'], (
-        'scaling for plaquette 3 didnt work')
-        assert mc.energy.scaling_for_plaq4 ==  mc_schedule['energy_scaling']['scaling_for_plaq4'], (
-        'scaling for plaquette 4 didnt work')
+# def check_update(energy: Energy, mc_schedule: dict):
+#     """check if upate from yaml is working"""
+#     mc = MC(deepcopy(energy))
+#     mc = update_mc(mc, mc_schedule)
+#     if 'energy_scaling' in list(mc_schedule.keys()):
+#         assert mc.energy.scaling_for_plaq3 ==  mc_schedule['energy_scaling']['scaling_for_plaq3'], (
+#         'scaling for plaquette 3 didnt work')
+#         assert mc.energy.scaling_for_plaq4 ==  mc_schedule['energy_scaling']['scaling_for_plaq4'], (
+#         'scaling for plaquette 4 didnt work')
 
