a4f9a0b6ce30e7c7c0afcf07e23d8ca9c8b900dc
Kerem <akd.kerem@gmail.com>
2022-05-09 11:31:28 +0200

    create nodes from graph (trivial) and create qbit to coord dict from given Node ordering

--- graph.py
+++ graph.py
@@ -12,17 +12,20 @@ class Graph:
         self.adj_matrix = adj_matrix
         self.N = self.num_nodes()
         self.K = self.num_edges()
-        self.C = self.num_constrains()
         self.clear()
         three_cycles, four_cycles = self.get_cycles(3), self.get_cycles(4)
-        self.number_of_3_cycles, self.number_of_4_cycles = len(three_cycles), len(four_cycles)
+        self.number_of_3_cycles, self.number_of_4_cycles = (
+            len(three_cycles),
+            len(four_cycles),
+        )
         self.cycles = three_cycles + four_cycles
         self.qbits = self.qbits_from_graph()
-        
+        self.nodes = self.nodes_from_graph()
+        self.C = self.num_constrains()
+
     def to_nx_graph(self):
         """converts Graph object to networkx object"""
         return nx.from_numpy_array(self.adj_matrix)
-    
 
     def clear(self):
         self.visited = [False] * self.N
@@ -84,10 +87,15 @@ class Graph:
     # TODO: count degeneracies D in graph
     def num_constrains(self):
         """C = K - N + 1"""
-        return int(self.K - self.N + 1)
+        return int(self.K - len(self.nodes) + 1)
 
+    # TODO: replace by len(self.nodes)
     def num_nodes(self):
         return int(self.adj_matrix.shape[0])
+
+    def nodes_from_graph(self):
+        """return nodes"""
+        return list({node for qubit in self.qbits for node in qubit})
 
     def num_edges(self):
         return int(self.adj_matrix.sum() // 2)
@@ -96,7 +104,7 @@ class Graph:
     def complete(cls, n):
         """generates complete connected graph with n nodes"""
         return cls(np.ones((n, n)) - np.identity(n))
-    
+
     @classmethod
     def init_without_edges(cls, n, edges_to_remove: list):
         """generate graph without edges in edges_to_remove"""
@@ -104,8 +112,6 @@ class Graph:
         for edge in edges_to_remove:
             adj_matrix[edge], adj_matrix[edge[::-1]] = 0, 0
         return cls(adj_matrix)
-       
-            
 
     # TODO: random graph may have no cycles, fix this-> check if C,K,N are compatibe, add density parameter
     @classmethod
@@ -114,3 +120,42 @@ class Graph:
         matrix = np.triu(np.random.randint(2, size=(n, n)), k=1)
         matrix += matrix.transpose()
         return cls(matrix)
+
+    def qbit_to_coord_from_nodes(
+        self, N_x, N_y, mirror_qbits=True, swap_symmetric=True
+    ):
+        """
+        returns dict which assigns
+        coords on physical grid to 
+        qbits, according to order of
+        nodes in N_x and N_y
+        if mirror_qbits is True, qbits like (1,8) and (8,1)
+        exists at the same time
+        if swap_symmetric, N_x/N_y are initialised in the same order
+        """
+        if N_x is None or N_y is None:
+            # create nodes and shuffle them
+            nodes = self.nodes[:]
+            np.random.shuffle(nodes)
+            N_x = nodes[:]
+            if not swap_symmetric:
+                np.random.shuffle(nodes)
+            N_y = nodes
+
+        nodes_x, nodes_y = np.meshgrid(N_x, N_y)
+        # create coords
+        x, y = np.meshgrid(np.arange(len(nodes_x)), np.arange(len(nodes_y)))
+        coords = list(zip(x.flatten(), y.flatten()))
+        qbits = list(zip(nodes_x.flatten(), nodes_y.flatten()))
+        if mirror_qbits:
+            valid_qbits = np.array(
+                [tuple(sorted(qbit)) in self.qbits for qbit in qbits]
+            )
+        else:
+            valid_qbits = np.array([tuple(qbit) in self.qbits for qbit in qbits])
+        qbit_to_coord_dict = {
+            qbit: coord
+            for valid, qbit, coord in zip(valid_qbits, qbits, coords)
+            if valid
+        }
+        return qbit_to_coord_dict
