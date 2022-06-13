1f9bb4c224952a5c993e56142039d1abb4fbb101
Kerem Akdogan <csba1344@fermion.uibk.ac.at>
2022-06-13 14:45:28 +0200

    update qbits by core

--- paths.py
+++ paths.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 cwd = (Path(__file__).parent.absolute())
 parameters_path = (cwd / "benchmark/parameters")
+logger_path = (cwd / "benchmark/logs")
 benchmark_results_path = (cwd / "benchmark/results")
 database_path = (cwd / "reversed_engineering/database")
 energy_scalings = (cwd / "objective_function/energy_scalings")
