4a20e1d657ee5746b3ce349d138a0ca41bbe0e7e
Kerem Akdogan <csba1344@fermion.uibk.ac.at>
2022-06-13 14:34:16 +0200

    simpler structure: argparser hands over benchmark settings which cant be set in the yaml, or make no sense there, problem graph is send to run_benchmarks in functions for benchmarking

--- __init__.py
+++ __init__.py
@@ -4,9 +4,12 @@ from .graph import Graph
 from .polygons import Polygons
 from .qbits import Qbits, Qbit
 from . import core
-from .objective_function import Energy
+from .objective_function import Energy, Energy_core
 from . import optimization
 from .optimization import MC, MC_core
 
+from .benchmark import functions_for_benchmarking
+from .benchmark import benchmark_optimization
 
+from . import benchmark
 from .reversed_engineering import functions_for_database
