779b6ad38e4332fcd89819a53f576817aed255d2
Kerem Akdogan <csba1344@fermion.uibk.ac.at>
2022-09-19 19:56:18 +0200

    benchmark with core on lhz graphs is specially treated

--- benchmark/functions_for_benchmarking.py
+++ benchmark/functions_for_benchmarking.py
@@ -95,6 +95,8 @@ def initialize_MC_object(graph: Graph, mc_schedule: dict, core: bool = False):
     else:
         energy = init_energy(graph)
         mc = MC(energy)
+        mc.with_core = mc_schedule["with_core"]
+
     mc = update_mc(mc, mc_schedule, core)
     mc.energy.set_scaling_from_model()
     mc.n_moves = int(mc.n_moves * mc.repetition_rate)
@@ -122,6 +124,7 @@ def evaluate_optimization(
     graph: Graph,
     mc_schedule: dict,
     args: "arguments from parser",
+    _id: str=str(uuid4()),
 ):
     """
     evaluate monte carlo/ simulated annealing schedule for batch_size timed and returns
@@ -129,33 +132,35 @@ def evaluate_optimization(
     configuration distribution, ...) is
     """
     mc = initialize_MC_object(graph, mc_schedule)
-    if mc_schedule["with_core"]:
-        mc_core = initialize_MC_object(graph, mc_schedule, core=True)
+    if mc.with_core:
+        if not graph.is_complete:
+            mc_core = initialize_MC_object(graph, mc_schedule, core=True)
     else:
         mc.swap_probability = 0
     for iteration in range(args.batch_size):
         # search for core
-        if mc_schedule["with_core"]:
+        if mc.with_core:
             (
                 qubit_coord_dict,
                 mc.energy.polygon_object.core_corner,
                 ancillas_in_core,
             ) = search_max_core(mc_core, graph.K)
-            # if core is empty, dont allow swaps
-            if len(qubit_coord_dict) == 0:
-                mc.swap_probability = 0
-                mc.finite_grid_size = True
-                mc.random_qbit = True
-            else:
-                save_object(mc_core, path_to_results(args) / f'{args.name}/{str(uuid4())}_CORE_N_K_C_{graph.N}_{graph.K}_{graph.C}_.pkl')
+            if not graph.is_complete:
+                # if core is empty, dont allow swaps
+                if len(qubit_coord_dict) == 0:
+                    mc.swap_probability = 0
+                    mc.finite_grid_size = True
+                    mc.random_qbit = True
+                else:
+                    save_object(mc_core, path_to_results(args) / f'{args.name}/{_id}_CORE_N_K_C_{graph.N}_{graph.K}_{graph.C}_.pkl')
 
-            # reset mc core
-            mc_core.reset(mc_core.T_0, remove_ancillas=True)
-            # update core (reset is part of update)
-            # remove ancillas which are already in graph, but not recognized due to removing of short lines in graph
-            # add ancillas first since updates_qbits_from_dict doesnt know them otherwise
-            ancillas_in_core = {qubit:coord for qubit, coord in ancillas_in_core.items() if qubit not in graph.qubits}
-            mc.add_ancillas(ancillas_in_core, update_energy=False)
+                # reset mc core
+                mc_core.reset(mc_core.T_0, remove_ancillas=True)
+                # update core (reset is part of update)
+                # remove ancillas which are already in graph, but not recognized due to removing of short lines in graph
+                # add ancillas first since updates_qbits_from_dict doesnt know them otherwise
+                ancillas_in_core = {qubit:coord for qubit, coord in ancillas_in_core.items() if qubit not in graph.qubits}
+                mc.add_ancillas(ancillas_in_core, update_energy=False)
             mc.update_qbits_from_dict(qubit_coord_dict, assign_to_core=True)
             mc.total_energy, mc.number_of_plaquettes = mc.energy(
                 mc.energy.polygon_object.nodes_object.qbits
@@ -170,7 +175,8 @@ def evaluate_optimization(
         mc.remove_ancillas(
             mc.energy.polygon_object.nodes_object.propose_ancillas_to_remove()
         )
-        save_object(mc, path_to_results(args) / f'{args.name}/{str(uuid4())}_N_K_C_{graph.N}_{graph.K}_{graph.C}_.pkl')
+        mc.name, mc.batch_size = args.name, args.batch_size
+        save_object(mc, path_to_results(args) / f'{args.name}/{_id}_N_K_C_{graph.N}_{graph.K}_{graph.C}_.pkl')
         # reset mc object
         mc.reset(current_temperature=mc.T_0, remove_ancillas=True, keep_core=False)
 
@@ -250,13 +256,22 @@ def visualize_settings(
                      number_of_images=args.number_of_images)
 
 
-def search_max_core(mc_core, K):
-    mc_core.apply(mc_core.n_moves)
-    (
-        core_qbit_to_core_dict,
-        core_corner,
-        ancillas_in_core,
-    ) = mc_core.energy.qbits_in_max_core(K)
+def search_max_core(mc_core, graph):
+    """
+    is graph is complete connected graph, do not search for the maximim merged graph,
+    but simply take the maximum complete bipartite core
+    """
+    if graph.is_complete:
+        core_qbit_to_core_dict = Polygons.move_to_center(graph.max_complete_bipartite_core_for_LHZ(), graph.K)
+        core_corner = Polygons.corner_of_coords(list(core_qbit_to_core_dict.values()))
+        ancillas_in_core = []
+    else:
+        mc_core.apply(mc_core.n_moves)
+        (
+            core_qbit_to_core_dict,
+            core_corner,
+            ancillas_in_core,
+        ) = mc_core.energy.qbits_in_max_core(graph.K)
     return core_qbit_to_core_dict, core_corner, ancillas_in_core
 
 
