80d5307bc5e835cfb6033c2a43b35a2b58d1a3e0
Kerem Akdogan <csba1344@fermion.uibk.ac.at>
2022-09-30 15:53:00 +0200

    deleted

--- benchmark/parameters/run_parameters.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-find -name "*yaml" | parallel python ../../benchmark_optimization.py --yaml_path={} -b=10 -minN=10 -maxN=15 --min_C=10 --max_C=40 --max_size=1 --problem_folder='problems_by_square_density/fsquare_density_of_0.9'
