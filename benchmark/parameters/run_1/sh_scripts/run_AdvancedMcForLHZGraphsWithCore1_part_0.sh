c8dffe5db1c223a9c66ecabaa1e0322aea31013b
Kerem Akdogan <csba1344@fermion.uibk.ac.at>
2023-01-18 19:22:24 +0100

    add parameters to plot offline

---
+++ benchmark/parameters/run_1/sh_scripts/run_AdvancedMcForLHZGraphsWithCore1_part_0.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+NUM_PARALLEL_JOBS=10 
+part=0
+name=AdvancedMcForLHZGraphsWithCore1
+CSV_FILE=/net/fermion/csba1344/CompilerQC/benchmark/parameters/run_${number}/csvs/${name}_part_${part}.csv
+tail -n +2 ${CSV_FILE} | parallel --progress --colsep ',' -j${NUM_PARALLEL_JOBS} python "../benchmark_optimization.py --yaml_path={1} --batch_size={2} --min_N={3} --max_N={4} --min_C={5} --max_C={6} --max_size={7} --problem_folder={8} --run=${number}"  
+            
---
+++ benchmark/parameters/run_2/sh_scripts/run_EnergyForLHZGraphs2_part_1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+NUM_PARALLEL_JOBS=15
+part=1
+name=EnergyForLHZGraphs2
+number=2
+CSV_FILE=/net/fermion/csba1344/CompilerQC/benchmark/parameters/run_${number}/csvs/${name}_part_${part}.csv
+tail -n +2 ${CSV_FILE} | parallel --progress --colsep ',' -j${NUM_PARALLEL_JOBS} python "/net/fermion/csba1344/CompilerQC/benchmark/benchmark_optimization.py --yaml_path={1} --batch_size={2} --min_N={3} --max_N={4} --min_C={5} --max_C={6} --max_size={7} --problem_folder={8} --run=${number}"  
+            
